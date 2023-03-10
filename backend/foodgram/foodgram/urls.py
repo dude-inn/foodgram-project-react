50e9968c1bc7d50783911f693e54adc49feda0ca
Михаил Дудин <dude-inn@yandex.ru>
2023-03-10 20:47:34 +0800

    10.03.23

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -10,11 +10,9 @@ urlpatterns = [
         include('api.urls', namespace='api')
     )
 ]
-urlpatterns += (
-            static(
-                settings.MEDIA_URL,
-                document_root=settings.MEDIA_ROOT
-            ) + static(
-                    settings.STATIC_URL,
-                    document_root=settings.STATIC_ROOT
-                ))
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT
+                      ) + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
